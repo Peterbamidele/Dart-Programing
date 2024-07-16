8ec27257ecaa71de47553e0fc2c5639a03a837a7
peter aro <peterbamidele98@gmail.com>
2024-07-16 19:06:54 +0100

    declearning varible and using it

---
+++ declearingvariables.dart
@@ -0,0 +1,36 @@
+void main() {
+  //Variables
+
+  // Strings
+  var name = "john";
+  //String name = "peter";
+   print(name);
+
+  // Interger
+  var x = 41;
+  // int x = 51;
+   print(x);
+
+  //Dynamic
+  dynamic firstname = "Bamidele";
+  print(firstname);
+
+  // Const and Final
+  const String fullName = "Peter Aro ";
+  final String nickName = "Bami-dele";
+  print(fullName);
+  print(nickName);
+
+  // declearling and not using
+  var myName;
+  print(myName);
+  
+  myName = "securitiesafrica";
+  print(myName);
+
+ 
+ 
+  
+  print(fullName);
+  print(nickName);
+}
