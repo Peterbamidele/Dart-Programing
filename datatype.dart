08420e730ce5c8097cf7a053ef56c05185428b56
peter aro <peterbamidele98@gmail.com>
2024-07-17 18:28:18 +0100

    datatype and forloop

---
+++ datatype.dart
@@ -0,0 +1,26 @@
+void main() {
+// Data Types
+// Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+// Strings
+  String firstName = "Peter";
+  print("String: $firstName");
+
+//Intergers
+  int myAge = 32;
+  print("Integer : $myAge");
+
+// Doubles
+  double otherNum = 32.1;
+  print("Double : $otherNum");
+
+// Booleans
+  bool myBool = false;
+  print("Boolean : $myBool");
+
+// Dynamic
+
+  dynamic saflMD = "Olamide";
+  saflMD = "Olanike";
+  print("SaflDeputyMD : $saflMD");
+}
