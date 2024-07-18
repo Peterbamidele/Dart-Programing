35f904e3c830247de35925782c33620f9b7f1e07
peter aro <peterbamidele98@gmail.com>
2024-07-18 14:32:04 +0100

    list add

---
+++ list.dart
@@ -0,0 +1,18 @@
+void main() {
+  List<String> saflName = [
+    'peter',
+    'ola',
+    'nike',
+    'olamide',
+    'peter-com',
+    'kayode',
+    'mary'
+  ];
+  // add name
+  saflName.add('azeeze');
+  // reomve
+  saflName.remove('mary');
+
+  saflName.add(40);
+  print(saflName);
+}
